b6f3b9bf39cc4bd429e4db51362f6acbba0be8db
unknown <omarrayes1998@gmail.com>
2025-01-05 22:01:02 +0300

    edit

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,16 +1,6 @@
 const express = require("express");
-const productController = require("./../controllers/productController");
-import { authMiddleware } from './../middleware/authMiddleware';
-import upload from '../middleware/productsPhotoUpload';
+
 const router = express.Router();
 
-router.route("/")
-      .get(authMiddleware,productController.getProducts)
-      .post(authMiddleware,upload.single('image'),productController.addProduct);
-
-router.route("/:id")
-      .get(authMiddleware,productController.getProduct)
-      .delete(authMiddleware,productController.deleteProducts)
-      .put(authMiddleware,upload.single('image'),productController.editProduct);
 
 export default router;
